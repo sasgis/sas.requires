ca6a30b51c556d57b98489017f6fb42027f85b3e
zed <starmen@tut.by>
2026-04-13 14:07:50 +0300

    Added VirtualTreeView component

---
+++ virtualtreeview/Source/VirtualTrees.DragImage.pas
@@ -0,0 +1,139 @@
+﻿unit VirtualTrees.DragImage;
+
+interface
+
+uses
+  WinApi.Windows,
+  WinApi.ActiveX,
+  System.Types,
+  Vcl.Controls,
+  Vcl.Graphics;
+
+{$MINENUMSIZE 1, make enumerations as small as possible}
+
+
+type
+  // Drag image support for the tree.
+  TVTTransparency = 0 .. 255;
+
+  // Simple move limitation for the drag image.
+  TVTDragMoveRestriction = (
+    dmrNone,
+    dmrHorizontalOnly,
+    dmrVerticalOnly
+  );
+
+  TVTDragImageStates = set of (
+    disHidden,                            // Internal drag image is currently hidden (always hidden if drag image helper interfaces are used).																															
+    disInDrag,                            // Drag image class is currently being used.
+    disPrepared                           // Drag image class is prepared.
+    );
+
+  // Class to manage header and tree drag image during a drag'n drop operation.
+  TVTDragImage = class
+  private
+    FOwner         : TCustomControl;
+    FBackImage,                              // backup of overwritten screen area
+    FAlphaImage,                             // target for alpha blending
+    FDragImage     : TBitmap;                // the actual drag image to blend to screen
+    FRestriction   : TVTDragMoveRestriction; // determines in which directions the drag image can be moved
+    FColorKey      : TColor;                 // color to make fully transparent regardless of any other setting
+    FStates        : TVTDragImageStates;     // Determines the states of the drag image class.
+  public
+    constructor Create(AOwner : TCustomControl);
+    destructor Destroy; override;
+
+    procedure EndDrag;
+    procedure PrepareDrag(DragImage : TBitmap; HotSpot : TPoint; const DataObject: IDataObject; pColorKey: TColor = clWindow);
+    property MoveRestriction : TVTDragMoveRestriction read FRestriction write FRestriction default dmrNone;
+  end;
+
+implementation
+
+uses
+  WinApi.ShlObj,
+  WinApi.Messages,
+  System.SysUtils,
+  System.Math,
+  VirtualTrees.DragnDrop,
+  VirtualTrees.Types,
+  VirtualTrees.Utils,
+  VirtualTrees.BaseTree;
+
+//----------------- TVTDragImage ---------------------------------------------------------------------------------------
+
+constructor TVTDragImage.Create(AOwner : TCustomControl);
+begin
+  FOwner := AOwner;
+  FRestriction := dmrNone;
+  FColorKey := clNone;
+end;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+destructor TVTDragImage.Destroy;
+begin
+  EndDrag;
+
+  inherited;
+end;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+procedure TVTDragImage.EndDrag;
+begin
+  FStates := FStates - [disInDrag, disPrepared];
+  FBackImage.Free;
+  FBackImage := nil;
+  FDragImage.Free;
+  FDragImage := nil;
+  FAlphaImage.Free;
+  FAlphaImage := nil;
+end;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+procedure TVTDragImage.PrepareDrag(DragImage : TBitmap; HotSpot : TPoint; const DataObject : IDataObject; pColorKey: TColor = clWindow);
+// Creates all necessary structures to do alpha blended dragging using the given image.
+// ImagePostion and HotSpot are given in screen coordinates. The first determines where to place the drag image while
+// the second is the initial mouse position.
+// This method also determines whether the system supports drag images natively. If so then only minimal structures
+// are created.
+
+var
+  Width, Height      : Integer;
+  DragSourceHelper   : IDragSourceHelper;
+  DragInfo           : TSHDragImage;
+  lDragSourceHelper2 : IDragSourceHelper2; // Needed to get Windows Vista+ style drag hints.
+  lNullPoint         : TPoint;
+begin
+  Width := DragImage.Width;
+  Height := DragImage.Height;
+  FColorKey := pColorKey;
+
+  // Determine whether the system supports the drag helper interfaces.
+  if Assigned(DataObject) and Succeeded(CoCreateInstance(CLSID_DragDropHelper, nil, CLSCTX_INPROC_SERVER, IDragSourceHelper, DragSourceHelper)) then
+  begin
+    lNullPoint := Point(0, 0);
+    if Supports(DragSourceHelper, IDragSourceHelper2, lDragSourceHelper2) then
+      lDragSourceHelper2.SetFlags(DSH_ALLOWDROPDESCRIPTIONTEXT); // Show description texts
+    // First let the system try to initialze the DragSourceHelper, this works fine for file system objects (CF_HDROP)
+    StandardOLEFormat.cfFormat := CF_HDROP;
+    if not Succeeded(DataObject.QueryGetData(StandardOLEFormat)) or not Succeeded(DragSourceHelper.InitializeFromWindow(0, lNullPoint, DataObject)) then
+    begin
+      // Supply the drag source helper with our drag image.
+      DragInfo.sizeDragImage.cx := Width;
+      DragInfo.sizeDragImage.cy := Height;
+      DragInfo.ptOffset := HotSpot;
+      DragInfo.hbmpDragImage := CopyImage(DragImage.Handle, IMAGE_BITMAP, Width, Height, LR_COPYRETURNORG);
+      DragInfo.crColorKey := ColorToRGB(FColorKey);
+      if not Succeeded(DragSourceHelper.InitializeFromBitmap(@DragInfo, DataObject)) then
+      begin
+        DeleteObject(DragInfo.hbmpDragImage);
+      end;
+    end;
+  end;
+end;
+
+
+end.
---
+++ virtualtreeview/Tests/VTOnEditCancelledTests.pas
@@ -0,0 +1,122 @@
+﻿unit VTOnEditCancelledTests;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  Vcl.Forms,
+  VirtualTrees;
+
+type
+
+  [TestFixture]
+  TVTOnEditCancelledTests = class
+  strict private
+    fTree: TVirtualStringTree;
+    fForm: TForm;
+    FEditCancelled: Boolean;
+    procedure TreeEditCancelled(Sender: TBaseVirtualTree; Column: TColumnIndex);
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure TestAddColumn;
+
+    [Test]
+    procedure TestEditNodeFail;
+
+    [Test]
+    procedure TestEditNode;
+
+    [Test]
+    procedure TestEditNodeReadOnly;
+
+    [Test]
+    procedure TestOnEditCancelled;
+  end;
+
+implementation
+
+uses
+  System.SysUtils, VirtualTrees.Types;
+
+procedure TVTOnEditCancelledTests.Setup;
+begin
+  fForm := TForm.Create(nil);
+  fTree := TVirtualStringTree.Create(fForm);
+end;
+
+procedure TVTOnEditCancelledTests.TearDown;
+begin
+  FreeAndNil(fForm);
+end;
+
+procedure TVTOnEditCancelledTests.TestAddColumn;
+begin
+  var LBeforeColumnCount := fTree.Header.Columns.Count;
+  fTree.Header.Columns.Add;
+  var LAfterColumnCount := fTree.Header.Columns.Count;
+  Assert.AreEqual<Integer>(LAfterColumnCount - LBeforeColumnCount, 1);
+end;
+
+procedure TVTOnEditCancelledTests.TestEditNode;
+begin
+  fForm.Show;
+  fTree.TreeOptions.MiscOptions := fTree.TreeOptions.MiscOptions + [toEditable];
+  fTree.Parent := fForm;
+  fTree.Header.Columns.Add;
+  var LNode := fTree.AddChild(fTree.RootNode);
+  var LEditNodeResult := fTree.EditNode(LNode, 0);
+  var LAfterStates := fTree.TreeStates;
+  Assert.AreEqual<TVirtualTreeStates>(LAfterStates * [tsEditing], [tsEditing]);
+  Assert.IsTrue(LEditNodeResult);
+end;
+
+procedure TVTOnEditCancelledTests.TestEditNodeFail;
+begin
+  fForm.Show;
+  fTree.TreeOptions.MiscOptions := fTree.TreeOptions.MiscOptions - [toEditable];
+  fTree.Parent := fForm;
+  fTree.Header.Columns.Add;
+  var LNode := fTree.AddChild(fTree.RootNode);
+  var LEditNodeResult := fTree.EditNode(LNode, 0);
+  Assert.IsFalse(LEditNodeResult);
+end;
+
+procedure TVTOnEditCancelledTests.TestEditNodeReadOnly;
+begin
+  fForm.Show;
+  fTree.Parent := fForm;
+  fTree.Header.Columns.Add;
+  var LNode := fTree.AddChild(fTree.RootNode);
+  fTree.TreeOptions.MiscOptions := fTree.TreeOptions.MiscOptions + [toReadOnly];
+  var LEditNodeResult := fTree.EditNode(LNode, 0);
+  Assert.IsFalse(LEditNodeResult);
+end;
+
+procedure TVTOnEditCancelledTests.TestOnEditCancelled;
+begin
+  fForm.Show;
+  FEditCancelled := False;
+  fTree.OnEditCancelled := TreeEditCancelled;
+  fTree.TreeOptions.MiscOptions := fTree.TreeOptions.MiscOptions + [toEditable];
+  var LNode := fTree.AddChild(fTree.RootNode);
+  fTree.Parent := fForm;
+  fTree.Header.Columns.Add;
+  fTree.EditNode(LNode, 0);
+  fTree.CancelEditNode;
+  Assert.IsTrue(FEditCancelled);
+end;
+
+procedure TVTOnEditCancelledTests.TreeEditCancelled(Sender: TBaseVirtualTree;
+  Column: TColumnIndex);
+begin
+  FEditCancelled := True;
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TVTOnEditCancelledTests);
+end.
